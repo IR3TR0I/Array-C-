3ed8807b5d2bc3bfa0ee7fa2bb90977dcb2c4404
Ruan <ruangustavo50711@gmail.com>
2020-11-19 17:28:24 -0300

    array lista de compras feito

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Array_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listaDeCompras = {"maçã", "batata","cenoura", "agua", "coca","Latão","Arroz","feijão","Salsinha","Salpicão"};
+            double[] precos = {1.1, 2.3, 4.5};
+            int[] idades = {16,17,17};
+
+            string[] nomesAlunos = new string[5];
+
+           // Console.WriteLine(listaDeCompras[1]);
+
+           // Console.WriteLine("trocar batata por qual item?");
+           // string item= Console.ReadLine();
+
+           // listaDeCompras[1] = Console.ReadLine();
+
+           //Console.WriteLine(listaDeCompras.Length);
+
+            for (int i = 0; i < listaDeCompras.Length; i++)
+            {
+                Console.WriteLine(listaDeCompras[i]);
+               // Console.WriteLine(i);
+               System.Threading.Thread.Sleep(1000);
+            }
+
+
+        }
+    }
+}
